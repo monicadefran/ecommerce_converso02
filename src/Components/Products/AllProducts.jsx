fc8a7ece94ac574595471947928aba6695dfb96d
Miguel <mseizduque@gmail.com>
2023-05-26 14:42:00 +0200

    AddToCart message

--- src/Components/Products/AllProducts.jsx
+++ src/Components/Products/AllProducts.jsx
@@ -1,37 +1,53 @@
-import React from "react";
-
-import {useContext} from "react";
-import {DataContext} from '../../Context/ConversoContext';
-
+import React, { useContext, useState } from "react";
+import { DataContext } from '../../Context/ConversoContext';
 import "./AllProducts.css";
 
-
 function AllProducts() {
-const { data, buyProduct } = useContext (DataContext);
+  const { data, buyProduct } = useContext(DataContext);
 
-   return (
-      <>
-   <div className="container-allProducts">
-{
-   data.map((product)=>{
-      return (
-         <div className="cartContent">
-            <div className="card" key={product.id}>
-            <div className="container-img"><img src={product.image} alt="product"/></div>
-            <div className="texto"><h3>{product.title}</h3>
-            <h4>€{product.price}</h4></div>
-            <button onClick={()=> buyProduct(product)}>Add To Cart</button>
-         </div>
-
-         </div>
-         
-
-      )
-   })
+  return (
+    <>
+      <div className="container-allProducts">
+        {data.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            buyProduct={buyProduct}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
-   </div>
-   </>
-   ) 
-};
+
+function ProductCard({ product, buyProduct }) {
+  const [showText, setShowText] = useState(false);
+
+  const handleBuyProduct = () => {
+    buyProduct(product);
+    setShowText(true);
+    setTimeout(() => {
+      setShowText(false);
+    }, 2000);
+  };
+
+  return (
+    <div className="cartContent">
+      <div className="card">
+        <div className="container-img">
+          <img src={product.image} alt="product" />
+        </div>
+        <div className="texto">
+          <h3>{product.title}</h3>
+          <h4>€{product.price}</h4>
+        </div>
+        <button className="move" onClick={handleBuyProduct}>
+          Add To Cart
+        </button>
+        {showText && <div>Producto agregado al carrito</div>}
+      </div>
+    </div>
+  );
+}
 
 export default AllProducts;
