12e58318111f246609a6dd57ca3364005c9054db
monicadefran <monicadefran@gmail.com>
2023-05-28 19:01:44 +0200

    Adding Jatna Like Icon

--- src/Components/Products/AllProducts.jsx
+++ src/Components/Products/AllProducts.jsx
@@ -8,31 +8,50 @@ import "./AllProducts.css";
 
 function AllProducts() {
 const { data, buyProduct } = useContext (DataContext);
+const [likedProducts, setLikedProducts] = useState([]);
+
+  const handleLike = (productId) => {
+    if (likedProducts.includes(productId)) {
+      setLikedProducts(likedProducts.filter((id) => id !== productId));
+    } else {
+      setLikedProducts([...likedProducts, productId]);
+    }
+  };
+
+  const getHeartEmoji = (productId) => {
+    if (likedProducts.includes(productId)) {
+      return "❤️";
+    } else {
+      return "🤍";
+    }
+  };
 
    return (
       <>
    <div className="container-allProducts">
-{
-   data.map((product)=>{
+   {data.map((product) => {
+      const isLiked = likedProducts.includes(product.id);
+
       return (
-         <div className="cartContent">
-            <div className="card" key={product.id}>
+        <div className="cartContent" key={product.id}>
+          <div className="card">
+            <span onClick={() => handleLike(product.id)} className="heart-icon-container">
+              <span className={isLiked ? 'heart-icon-red' : 'heart-icon'}>
+                {getHeartEmoji(product.id)}
+              </span>
+            </span>
             <div className="container-img">
-               <Link to={`/Details/${product.id}`}>
-                  <img src={product.image} alt="product"/>
-               </Link>
+              <img src={product.image} alt="product" />
             </div>
-            <div className="texto"><Link><h3>{product.title}</h3></Link>
-            <h4>€{product.price}</h4></div>
-            <button onClick={()=> buyProduct(product)}>Add To Cart</button>
-         </div>
-
-         </div>
-         
-
-      )
-   })
-}
+            <div className="texto">
+              <h3>{product.title}</h3>
+              <h4>€{product.price}</h4>
+            </div>
+            <button onClick={() => buyProduct(product)}>Add To Cart</button>
+          </div>
+        </div>
+      );
+    })}
    </div>
    </>
    ) 
