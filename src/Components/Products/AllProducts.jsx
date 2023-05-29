2d86a571eaca0bf1946bbab0014ef1983cb3a82c
Lucia <luludoesnfts@gmail.com>
2023-05-29 14:45:45 +0200

    Added search bar

--- src/Components/Products/AllProducts.jsx
+++ src/Components/Products/AllProducts.jsx
@@ -1,14 +1,12 @@
-
 import React, { useContext, useState } from "react";
-import { DataContext } from '../../Context/ConversoContext';
-import { Link } from "react-router-dom";
+import { DataContext } from "../../Context/ConversoContext";
+import { Link, useLocation } from "react-router-dom"; 
 
 import "./AllProducts.css";
 
-
 function AllProducts() {
-const { data, buyProduct } = useContext (DataContext);
-const [likedProducts, setLikedProducts] = useState([]);
+  const { data, buyProduct } = useContext(DataContext);
+  const [likedProducts, setLikedProducts] = useState([]);
 
   const handleLike = (productId) => {
     if (likedProducts.includes(productId)) {
@@ -26,38 +24,93 @@ const [likedProducts, setLikedProducts] = useState([]);
     }
   };
 
-   return (
-      <>
-   <div className="container-allProducts">
-   {data.map((product) => {
-      const isLiked = likedProducts.includes(product.id);
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const searchQuery = queryParams.get("search");
+  const filteredProducts = data.filter((product) => {
+    return product.title.toLowerCase().includes(searchQuery?.toLowerCase());
+  });
 
-      return (
-        <div className="cartContent" key={product.id}>
-          <div className="card">
-            <span onClick={() => handleLike(product.id)} className="heart-icon-container">
-              <span className={isLiked ? 'heart-icon-red' : 'heart-icon'}>
-                {getHeartEmoji(product.id)}
-              </span>
-            </span>
+  return (
+    <>
+      <div className="container-allProducts">
+        {searchQuery || filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
+            <div className="cartContent" key={product.id}>
+              <div className="card">
+                <span
+                  onClick={() => handleLike(product.id)}
+                  className="heart-icon-container"
+                >
+                  <span
+                    className={
+                      likedProducts.includes(product.id)
+                        ? "heart-icon-red"
+                        : "heart-icon"
+                    }
+                  >
+                    {getHeartEmoji(product.id)}
+                  </span>
+                </span>
 
-            <div className="container-img">
-               <Link to={`/Details/${product.id}`}>
-                  <img src={product.image} alt="product"/>
-               </Link>
+                <div className="container-img">
+                  <Link to={`/Details/${product.id}`}>
+                    <img src={product.image} alt="product" />
+                  </Link>
+                </div>
+                <div className="texto">
+                  <Link to={`/Details/${product.id}`}>
+                    <h3>{product.title}</h3>
+                  </Link>
+                  <h4>€{product.price}</h4>
+                </div>
+                <button onClick={() => buyProduct(product)}>
+                  Add To Cart
+                </button>
+              </div>
             </div>
-            <div className="texto">
-               <Link><h3>{product.title}</h3></Link>
-               <h4>€{product.price}</h4>
+          ))
+        ) : (
+          data.map((product) => (
+            <div className="cartContent" key={product.id}>
+              <div className="card">
+                <span
+                  onClick={() => handleLike(product.id)}
+                  className="heart-icon-container"
+                >
+                  <span
+                    className={
+                      likedProducts.includes(product.id)
+                        ? "heart-icon-red"
+                        : "heart-icon"
+                    }
+                  >
+                    {getHeartEmoji(product.id)}
+                  </span>
+                </span>
+
+                <div className="container-img">
+                  <Link to={`/Details/${product.id}`}>
+                    <img src={product.image} alt="product" />
+                  </Link>
+                </div>
+                <div className="texto">
+                  <Link to={`/Details/${product.id}`}>
+                    <h3>{product.title}</h3>
+                  </Link>
+                  <h4>€{product.price}</h4>
+                </div>
+                <button onClick={() => buyProduct(product)}>
+                  Add To Cart
+                </button>
+              </div>
             </div>
-            <button onClick={() => buyProduct(product)}>Add To Cart</button>
-          </div>
-        </div>
-      );
-    })}
-   </div>
-   </>
-   ) 
-};
+          ))
+        )}
+      </div>
+    </>
+  );
+}
 
 export default AllProducts;
+
